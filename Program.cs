be08903988c49e7353d45e9d5c05ce3b98b5543b
Btond <Btond@LAPTOP-K8U30UIB>
2020-02-04 21:13:24 -0500

    added system.io

--- Program.cs
+++ Program.cs
@@ -57,6 +57,13 @@ namespace InClassAssignment3
             //showing enums
             int myNum = (int)Months.April;
             Console.WriteLine(myNum);
+
+            //showing how to use system.io:
+            string writeText = "Hello World!";  // Create a text string
+            File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
+
+            string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content
         }
     }
     class Car
